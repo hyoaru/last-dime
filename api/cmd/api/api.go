e1c51674b1d11a8ff6984ecbe8ffe522e2592aa3
hyoaru <jjcabreraaaa@gmail.com>
2025-12-03 14:42:41 +0800

    Initial commit

---
+++ api/cmd/api/api.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type config struct {
+	addr string
+}
+
+type application struct {
+	config config
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("Server has started at %s", srv.Addr)
+	return srv.ListenAndServe()
+}
---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/hyoaru/last-dime
+
+go 1.25.4
